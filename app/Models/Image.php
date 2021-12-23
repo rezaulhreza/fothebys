2ce892371d00e83332a50f81a66146c1a63b7e80
rezaulhreza <56729226+rezaulhreza@users.noreply.github.com>
2021-12-23 01:47:09 +0000

    multi image upload-attemt failed-values not saved on db. Moving to single image

---
+++ app/Models/Image.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Image extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'lot_items_id',
+        'photo_name'
+    ];
+
+    public function lot()
+    {
+        return $this->belongsTo(LotItem::class, 'lot_items_id', 'id');
+    }
+}
---
+++ database/factories/LotItemFactory.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class LotItemFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $title = $this->faker->unique()->words($nb = 2, $asText = true);
+        $status=true;
+       
+        
+        return [
+            //
+        'title' => $title,
+      
+        'category_id' => $this->faker->numberBetween(1, 8),
+        'lot_ref' => $this->faker->numberBetween(1, 5),
+        'artist' => $this->faker->text(50),
+        'year' => $this->faker->year('now','-2 year'),
+       
+        'desc' => $this->faker->text(100),
+        'additional' => $this->faker->text(100),
+        'estimated' => $this->faker->numberBetween(10000,'-', 50000000),
+        'minimum' => $this->faker->numberBetween(10000,'-', 50000000),
+
+        'start_date' => $this->faker->dateTime('-1 week', '-2 month'),
+        'end_date' => $this->faker->dateTimeBetween('+1 week', '+5 month'),
+       
+        'status' => $status,
+
+        ]; 
+    }
+}
